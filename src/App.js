233e4b796299d007e84a0e0a55f7fcd375204344
NitAishwarya <aishwaryachhapparghare2803@gmail.com>
2024-06-12 20:28:42 +0530

    check 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClientPage from './Component/ClientPage/ClientPage';
+
+function App() {
+  return (
+    <div>
+
+      <ClientPage/>
+   
+    </div>
+  );
+}
+
+export default App;
